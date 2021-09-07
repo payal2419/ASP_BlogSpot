9913835876b41d655f17f97f235e57a3c638b1a8
payal2419 <payal.s.pawar@gmail.com>
2021-09-07 20:34:46 +0530

    Add project files.

---
+++ Controllers/PagesController.cs
@@ -0,0 +1,61 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ASP_BlogSpot.Controllers
+{
+    public class PagesController : Controller
+    {
+        public IActionResult Blog()
+        {
+            return View();
+        }
+
+        public IActionResult WhatsTrending()
+        {
+            return View();
+        }
+
+        public IActionResult Experiences()
+        {
+            return View();
+        }
+
+        public IActionResult Contact()
+        {
+            return View();
+        }
+
+        public IActionResult AddBlog()
+        {
+            return View();
+        }
+
+        public IActionResult Login()
+        {
+            return View();
+        }
+
+        public IActionResult AddNewBlog()
+        {
+            return View();
+        }
+
+        public IActionResult UserRegistration()
+        {
+            return View();
+        }
+
+        public IActionResult CreateBlog()
+        {
+            return View();
+        }
+
+        public IActionResult ShowBlog()
+        {
+            return View();
+        }
+    }
+}
---
+++ wwwroot/BlogSpot/js/Blog/app.js
@@ -0,0 +1,75 @@
+﻿var mybutton = document.getElementById('myBtn');
+
+window.onscroll = function () {
+    scrollFunction();
+}
+window.onload = function () {
+    document.getElementById("loading").style.display = 'block';
+    setTimeout(showData, 1000);
+
+}
+
+function scrollFunction() {
+
+    if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+        mybutton.style.display = "block";
+    }
+    else {
+        mybutton.style.display = "none";
+    }
+}
+
+document.getElementById('myBtn').addEventListener('click', function () {
+    document.body.scrollTop = 0;
+    document.documentElement.scrollTop = 0;
+})
+
+
+function showData() {
+    document.getElementById("loading").style.display = 'none';
+    document.getElementById("content-body").style.display = 'block';
+}
+
+document.getElementById('lgBtn').addEventListener('click', function () {
+
+    let username = document.getElementById('txtId').value;
+    let password = document.getElementById('txtPwd').value;
+
+
+    if (username == "" || username == null) {
+        document.getElementById('txtId').classList.add('is-invalid');
+        return false;
+    }
+
+    if (password == "" || password == null) {
+        document.getElementById('txtPwd').classList.add('is-invalid');
+        return false;
+    }
+
+    if (username == "user" && password == "123") {
+        document.getElementById('txtId').classList.remove('is-invalid');
+        document.getElementById('txtPwd').classList.remove('is-invalid');
+        reDirect();
+    }
+    else if (username == "admin" && password == "admin") {
+        document.getElementById('txtId').classList.remove('is-invalid');
+        document.getElementById('txtPwd').classList.remove('is-invalid');
+        reDirectAdmin(username, password);
+    }
+    else {
+        alert("Login Falied");
+        document.getElementById('txtId').value = "";
+        document.getElementById('txtPwd').value = "";
+    }
+
+
+
+})
+
+function reDirect() {
+    window.location.href = '/Pages/CreateBlog';
+}
+var u, p
+function reDirectAdmin(u, p) {
+    window.location.href = '/Admin/Index';
+}
